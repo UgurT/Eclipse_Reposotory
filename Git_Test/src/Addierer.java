46c77db26e67fec006d82ee11364800bd3387076
UgurT <UgurTunali@online.de>
2015-01-14 10:53:16 +0100

    Addierer

---
+++ Git_Test/src/Addierer.java
@@ -0,0 +1,12 @@
+
+public class Addierer {
+
+	public int add(int x, int y){
+		return x +y;
+	}
+	public static void main(String[] args) {
+		
+		Addierer a = new Addierer();
+		System.out.println(a.add(5, 4));
+	}
+}
