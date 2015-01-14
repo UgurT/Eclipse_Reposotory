8ca02b07dcb2d9e25596eaec135f2ef0931c2892
UgurT <UgurTunali@online.de>
2015-01-14 10:57:18 +0100

    test12

--- Git_Test/src/Addierer.java
+++ Git_Test/src/Addierer.java
@@ -1,12 +1,12 @@
 
 public class Addierer {
 
-	public int add(int l, int y){
-		return l +y;
+	public int sub(int l, int y){
+		return l -y;
 	}
 	public static void main(String[] args) {
 		
 		Addierer a = new Addierer();
-		System.out.println(a.add(5, 9));
+		System.out.println(a.sub(5, 9));
 	}
 }
